5ac2c9bfdf3803ae33ce7ca2183f9e304f8be7fd
KhieuDuong <khieu.duongvan@gmail.com>
2022-12-05 16:24:04 +0900

    edit file server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8082"
+  origin: "https://frontend-reactjs-xi.vercel.app"
 };
 
 app.use(cors(corsOptions));
